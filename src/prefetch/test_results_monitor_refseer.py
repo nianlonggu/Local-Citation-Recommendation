8484603c5cdd75b94b82864227c3bf7b2f2d4d60
Nianlong Gu <nianlonggu@gmail.com>
2022-11-07 00:38:29 +0100

    Delete test_results_monitor_refseer.py

--- src/prefetch/test_results_monitor_refseer.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-
-subprocess.run(  "bash compute_papers_embedding.sh 640000 80000 config/refseer/compute_papers_embedding.config computing_embedding_refseer.temp ../../embedding/prefetch/refseer/paper_embedding.pkl_ ../../embedding/prefetch/refseer/paper_embedding.pkl".split()  )
-subprocess.run( "python test.py -config_file_path  config/refseer/test_NN.config  -start 0 -size 10000".split() )
-
-
